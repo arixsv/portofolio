5ffcbf5d2ca3f21905b45638f2285ad1e6545491
Ari Bagus Hidayat <61731040+arixsv@users.noreply.github.com>
2023-09-15 08:08:12 +0700

    Add files via upload

---
+++ portofolio_aribagus/assets/js/justread.js
@@ -0,0 +1,8 @@
+// Anti Copy Paste
+if (typeof document.onselectstart != "undefined") {
+    document.onselectstart = new Function("return false");
+}
+else {
+    document.onmousedown = new Function("return false");
+    document.onmouseup = new Function("return true");
+}
